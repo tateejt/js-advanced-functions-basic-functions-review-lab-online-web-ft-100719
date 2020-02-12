28118b0cb8cf1008eda7a3082ca3395f0c2a9deb
Emery Tate <tateejt@gmail.com>
2020-02-12 04:18:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function saturdayFun(activity="roller-skate") {
-  return `This Saturday, I want to ${activity}!`
-}
+// function saturdayFun(activity="roller-skate") {
+//   return `This Saturday, I want to ${activity}!`
+// }
 
-let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
-}
+// let mondayWork = function(activity="go to the office") {
+//   return `This Monday, I will ${activity}.`
+// }
 
 let wrapAdjective = function(style="*") {
   return function(adjective="special") {
@@ -12,27 +12,27 @@ let wrapAdjective = function(style="*") {
   }
 }
 
-const Calculator = {
-  add: function(a,b) {
-    return a + b;
-  },
-  subtract: function(a,b) {
-    return a - b
-  },
-  multiply: function(a,b) {
-    return a * b
-  },
-  divide: function(a,b) {
-    return a / b;
-  }
-}
+// const Calculator = {
+//   add: function(a,b) {
+//     return a + b;
+//   },
+//   subtract: function(a,b) {
+//     return a - b
+//   },
+//   multiply: function(a,b) {
+//     return a * b
+//   },
+//   divide: function(a,b) {
+//     return a / b;
+//   }
+// }
 
-let actionApplyer = function(start, ray) {
-  let a = start
+// let actionApplyer = function(start, ray) {
+//   let a = start
 
-  for (let i = 0; i < ray.length; i++ ){
-    a = ray[i](a)
-  }
+//   for (let i = 0; i < ray.length; i++ ){
+//     a = ray[i](a)
+//   }
 
-  return a
-}
+//   return a
+// }
